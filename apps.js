e67b83ac739098ed23aa676c0973b1ac8d8c0638
Krishna Garg <kgarg5293@gmail.com>
2024-08-02 09:49:19 +0000

    changes completed

--- apps.js
+++ apps.js
@@ -1,1 +1,11 @@
+account_Email ="kg@gmail.com"
+accountPassword="14421"
+accountcity = "Gujarat"
 
+console.log(account_Email);
+console. table([account_Email,accountPassword,accountcity]);
+
+/*prefer not to use var
+because of issues in block scope and fuctional scope
+*/
+ 
